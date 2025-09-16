12a19d9d97968efc7dee94b915689619280dbc0e
Judah Lesser <judahlesser@Judahs-MacBook-Air.local>
2025-09-15 20:31:25 -0400

    add more chrome clear out

--- start.sh
+++ start.sh
@@ -6,10 +6,9 @@ export XDG_RUNTIME_DIR=/run/user/1000
 
 unclutter -idle 1 -root &
 
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/$USER/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/$USER/.config/chromium/Default/Preferences
+rm -rf /home/$USER/.cache/chromium && rm -rf /home/$USER/.config/chromium
 
 echo "$(</home/admin/Documents/HillelDepartureBoard/crontab.txt)" | crontab -
 source /home/admin/Documents/HillelDepartureBoard/venv/bin/activate
 cd /home/admin/Documents/HillelDepartureBoard/; ./arrivals.py --marc_code 12018-12015 --metro_code E09 --refresh 20 &
-chromium-browser --noerrdialogs --disable-infobars --kiosk --no-first-run file:///home/admin/Documents/HillelDepartureBoard/DepartureBoard.html &
+chromium-browser --noerrdialogs --disable-infobars --kiosk --no-crash-upload --disable-breakpad --disable-crash-reporter --incognito --disable-translate --no-first-run file:///home/admin/Documents/HillelDepartureBoard/DepartureBoard.html &
--- stop.sh
+++ stop.sh
@@ -5,5 +5,4 @@ export WAYLAND_DISPLAY=wayland-0
 export XDG_RUNTIME_DIR=/run/user/1000
 /usr/bin/wlr-randr --output HDMI-A-1 --off
 
- cd /home/admin/Documents/HillelDepartureBoard; git pull
- 
+cd /home/admin/Documents/HillelDepartureBoard; git pull
